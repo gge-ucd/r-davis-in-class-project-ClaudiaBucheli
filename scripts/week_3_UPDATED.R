3122df5b99f54d7f3e6e2fe85a008f86a5922336
ClaudiaBucheli <91276828+ClaudiaBucheli@users.noreply.github.com>
2021-10-06 21:49:19 -0700

    Update week_3_UPDATED.R

---
+++ scripts/week_3_UPDATED.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+surveys_t <- read.csv("data/portal_data_joined.csv")
+surveys_t
+
+
+surveys_base <- surveys_t[c("species_id","weight","plot_type")];surveys_base
+
+
+surveys_base <- surveys_base[1:60,]; surveys_base
+
+
+surveys_base <- as.character(surveys_base$species_id)
+
+surveys_base <- as.character(surveys_base$plot_type)
+
+surveys_base <- surveys_base[complete.cases(surveys_base),];surveys_base
