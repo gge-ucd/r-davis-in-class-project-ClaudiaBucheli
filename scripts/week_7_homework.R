d91207c1705f2a88214b3b061acc99aed042a93c
cjbucheli@ucdavis.edu <cjbucheli@ucdavis.edu>
2021-11-17 23:09:29 -0800

    update

--- scripts/week_7_homework.R
+++ scripts/week_7_homework.R
@@ -1,15 +1,21 @@
 library(tidyverse)
+library (LaCroixColoR)
 
 gapminder <- read_csv("https://gge-ucd.github.io/R-DAVIS/data/gapminder.csv")
 
 popg <- gapminder %>% 
   select(pop, country, continent, year) %>% 
-  filter(year > 2002) %>%
+  filter(year >= 2002) %>%
+  filter(continent != "Oceania") %>% 
   pivot_wider(names_from = year, values_from = pop) %>%
-  mutate(pop_dif = 2007 - 2002)
+  mutate(pop_dif = `2007` - `2002`)
 
-popg %>% 
-filter(continent != "Oceania") %>% 
-ggplot(aes(x = order_by(country), y = popg)) +
-  geom_col(aes(fill = continent)) + 
-  
+
+  ggplot(data = popg, aes(x = reorder(country, pop_dif), y = pop_dif)) +
+  geom_col(aes(fill = continent)) +
+  facet_wrap(~continent, scales = "free") +
+  scale_y_continuous(name = 'Population growth between 2002 and 2007') +
+  scale_x_discrete(name ='Countries by continent') +
+  theme_grey() +
+  scale_color_gradient(low="blue", high="red") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
