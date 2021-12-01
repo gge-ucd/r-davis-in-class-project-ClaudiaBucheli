367c24555c9e4ca4490354854942167ab6e2ca48
cjbucheli@ucdavis.edu <cjbucheli@ucdavis.edu>
2021-11-30 22:10:27 -0800

    .

--- scripts/week_9_homework.R
+++ scripts/week_9_homework.R
@@ -1,10 +1,6 @@
 library(tidyverse)
+
 library(lubridate)
-
-
-
-
-
 
 
 mloa <-read_csv("https://raw.githubusercontent.com/gge-ucd/R-Davis/master/data/mauna_loa_met_2001_minute.csv")
@@ -14,9 +10,10 @@ mloa1 <- mloa %>%
   filter(rel_humid != -99) %>% 
   filter(temp_C_2m != -999.99) %>% 
   filter(windSpeed_m_s !=-999.99) %>% 
-  mutate(datetime = ymd_hm(paste0("year", "-", "month", "-", "day","-","hour24", ":",
-                                  "min", ":"),tz="UTC")) %>%
+  mutate(datetime = ymd_hm(paste0(year, "-", month, "-", day,"-", hour24, ":",
+                                  min),tz="UTC")) %>%
   mutate(datetimeLocal = with_tz(datetime,tz = "Pacific/Honolulu"))
+
 
   
          
